e92b7942ea9e0e6c10fc84a12fbb657c6223bf39
Victor <victor.nyoike@strathmore.edu>
2026-05-18 12:32:15 +0300

    css

---
+++ GTA6/js/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("GTA VI Fan Page Loaded Successfully!");
+
+    const title = document.getElementById('mainHeading');
+    title.addEventListener('click', () => {
+        alert("Get ready for Vice City 2025!");
+    });
+});
